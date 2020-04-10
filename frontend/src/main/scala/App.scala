3c0d69fc3f29329246adbca42fbe33fbf8329453
ManishGowardipe <gowardipemanish95@gmail.com>
2020-04-10 15:56:10 +0530

    integrated with backend

--- frontend/src/main/scala/App.scala
+++ frontend/src/main/scala/App.scala
@@ -1,26 +1,30 @@
-import components.Printer
-import models.MessageCodec
+import http.Client
+import io.bullet.borer.Json
+import models.{Message, MessageCodec}
 import org.scalajs.dom.document
 import slinky.web.ReactDOM
+import components.Printer
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object App extends MessageCodec {
 
   def main(args: Array[String]): Unit = {
     {
       var text = ""
-//      Client
-//        .get("http://localhost:9000/hello")
-//        .onComplete {
-//          case Success(response) =>
-//            val message: Message =
-//              Json.decode(response.responseText.getBytes()).to[Message].value
-//            text = message.value
-//          case Failure(exception) =>
-//            text = exception.getMessage
-//        }
+      Client
+        .get("http://localhost:9000/hello")
+        .onComplete {
+          case Success(response) =>
+            val message: Message =
+              Json.decode(response.responseText.getBytes()).to[Message].value
+            text = message.value
+            val container = document.getElementById("container")
+            ReactDOM.render(Printer(text), container)
+          case Failure(exception) =>
+            text = exception.getMessage
+        }
 
-      val container = document.getElementById("container")
-      ReactDOM.render(Printer("string from stateless component"), container)
     }
   }
 }
